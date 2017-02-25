9a63a4b55a5aa01ec24384b17ee7392c2bd14372
Peng Sun <pengsun000@gamil.com>
2017-02-25 16:24:04 +0800

    make dependency of gym_tvizdoom optional in envs.py

--- envs.py
+++ envs.py
@@ -1,16 +1,25 @@
 import cv2
-from gym.spaces.box import Box
 import numpy as np
-import gym
-import gym_tvizdoom
+
+from gym.spaces.box import Box
 from gym import spaces
-import logging
+import gym
+
+try:
+    import gym_tvizdoom
+    has_gym_tvizdoom = True
+except ImportError:
+    has_gym_tvizdoom = False
+
 import universe
 from universe import vectorized
 from universe.wrappers import BlockingReset, GymCoreAction, EpisodeID, Unvectorize, Vectorize, Vision, Logger
 from universe import spaces as vnc_spaces
 from universe.spaces.vnc_event import keycode
+
+import logging
 import time
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,12 +29,14 @@ universe.configure_logging()
 def create_env(env_id, client_id, remotes, **kwargs):
     spec = gym.spec(env_id)
 
+    if has_gym_tvizdoom:
+        if spec.tags.get('vizdoom', False):
+            return create_vizdoom_env(env_id)
+
     if spec.tags.get('flashgames', False):
         return create_flash_env(env_id, client_id, remotes, **kwargs)
     elif spec.tags.get('atari', False) and spec.tags.get('vnc', False):
         return create_vncatari_env(env_id, client_id, remotes, **kwargs)
-    elif spec.tags.get('vizdoom', False):
-        return create_vizdoom_env(env_id)
     else:
         # Assume atari.
         assert "." not in env_id  # universe environments have dots in names.
